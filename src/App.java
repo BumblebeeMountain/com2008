ae283ae5f936e549d0ec51956b778db519c72f39
Dom Barter <dom@hexos.co.uk>
2020-10-27 10:24:53 +0000

    Created base java environment

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nDrivers loaded as properties:");
+        System.out.println(System.getProperty("jdbc.drivers"));
+        System.out.println("\nDrivers loaded by DriverManager:");
+        Enumeration list = DriverManager.getDrivers();
+        while (list.hasMoreElements())
+            System.out.println(list.nextElement());
+    }
+}
