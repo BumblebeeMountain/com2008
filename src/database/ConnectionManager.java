4d7e07215eb8838b7684ee70e9f47357a4dd9318
Bumblebee Mountain <BumblebeeMountain@gmail.com>
2020-11-04 10:20:43 +0000

    Implement connection manager class

--- src/database/ConnectionManager.java
+++ src/database/ConnectionManager.java
@@ -3,7 +3,20 @@
 //  This wrapper will also be the central point for the login details of the db
 
 package database;
+import java.sql.*;
 
 public class ConnectionManager {
-    
+    /**
+     * getConnection()
+     * Static function to get a new connection object from the group SQL database
+     * @param null
+     * @return Connection, the created connection object
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:mysql://stusql.dcs.shef.ac.uk/team019",
+                "team019",
+                "21b87683");
+    }
 }
