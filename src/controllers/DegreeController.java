22fde38fe7608cdadad11deb4b65cc2ef8c92159
Dom Barter <dom@hexos.co.uk>
2020-11-09 10:41:33 +0000

    Refactor is complete

--- src/controllers/DegreeController.java
+++ src/controllers/DegreeController.java
@@ -5,63 +5,43 @@ import exceptions.GeneralProcessingException;
 import exceptions.NoRecordException;
 import models.Degree;
 import models.DegreeModule;
-import models.Department;
 
 public class DegreeController {
-    
-    public static Degree[] getAllDegrees()
-    throws GeneralProcessingException {
+
+    public static Degree[] getAllDegrees() throws GeneralProcessingException {
         return null;
     }
 
-    public static Degree getDegree(String degreeCode)
-    throws GeneralProcessingException, NoRecordException {
+    public static Degree getDegree(String degreeCode) throws GeneralProcessingException, NoRecordException {
         return null;
     }
 
     public static void createDegree(String degreeCode, String degreeName, Boolean hasYearInIndustry, Integer maxLevel)
-    throws GeneralProcessingException, ExistingRecordException {
+            throws GeneralProcessingException, ExistingRecordException {
 
     }
 
-    public static void removeDegree(String degreeCode)
-    throws GeneralProcessingException {
-        
+    public static void removeDegree(String degreeCode) throws GeneralProcessingException {
+
     }
 
-    public static DegreeModule[] getAllDegreeModules()
-    throws GeneralProcessingException {
+    public static DegreeModule[] getAllDegreeModules() throws GeneralProcessingException {
         return null;
     }
 
-    public static DegreeModule[] getDegreeModules (String degreeCode)
-    throws GeneralProcessingException {
+    // This is private as it is only for internal checks
+    private static DegreeModule getDegreeModule(String degreeCode, String moduleCode)
+            throws GeneralProcessingException, NoRecordException {
         return null;
     }
 
-    public static DegreeModule getDegreeModule (String degreeCode, String moduleCode)
-    throws GeneralProcessingException, NoRecordException {
-        return null;
-    }
-
-    public static void createDegreeModule (String degreeCode, String moduleCode, Boolean core)
-    throws GeneralProcessingException, ExistingRecordException {
+    public static void createDegreeModule(String degreeCode, String moduleCode, Boolean core)
+            throws GeneralProcessingException, ExistingRecordException {
 
     }
 
-    public static void removeDegreeModule (String degreeCode, String moduleCode)
-    throws GeneralProcessingException {
-        
-    }
+    public static void removeDegreeModule(String degreeCode, String moduleCode) throws GeneralProcessingException {
 
-    public static Department getLeadDepartment (String degreeCode)
-    throws GeneralProcessingException, NoRecordException {
-        return null;
-    }
-
-    public static Department[] getPartnerDepartments (String degreeCode)
-    throws GeneralProcessingException {
-        return null;
     }
 
 }
