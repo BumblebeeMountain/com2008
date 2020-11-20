a7667e2e27c7fb61dbe7fe1823f7d5087dd6dca3
Dom Barter <dom@hexos.co.uk>
2020-11-20 10:04:49 +0000

    Clean up student sign up

--- src/views/StudentSignUp.java
+++ src/views/StudentSignUp.java
@@ -22,10 +22,12 @@ public class StudentSignUp extends JPanel {
 
         // Set the JComboBoxes
         Constants.Title[] titles = Constants.Title.values();
-        for (Constants.Title t : titles) titleComboBox.addItem(t.toString());
+        for (Constants.Title t : titles)
+            titleComboBox.addItem(t.toString());
         try {
             Degree[] degrees = DegreeController.getAllDegrees(true);
-            for (Degree d : degrees) degreeComboBox.addItem(d.getCode());
+            for (Degree d : degrees)
+                degreeComboBox.addItem(d.getCode());
         } catch (Exception e) {
             this.rootFrame.moveToRegistrarDashboard(); // Error - go back
         }
@@ -40,20 +42,24 @@ public class StudentSignUp extends JPanel {
     }
 
     private void createStudentButtonActionPerformed(ActionEvent e) {
-        String title = (String)titleComboBox.getSelectedItem();
+        String title = (String) titleComboBox.getSelectedItem();
         String forename = forenameBox.getText().trim();
         String surname = surnameBox.getText().trim();
         String password = new String(passwordBox.getPassword());
         String personalTutor = personalTutorBox.getText().trim();
-        String degreeCode = (String)degreeComboBox.getSelectedItem();
+        String degreeCode = (String) degreeComboBox.getSelectedItem();
 
-        if (title.equals("") || forename.equals("") || surname.equals("") || password.equals("") || personalTutor.equals("") || degreeCode.equals("")) {
+        if (title.equals("") || forename.equals("") || surname.equals("") || password.equals("")
+                || personalTutor.equals("") || degreeCode.equals("")) {
             rootFrame.showError("Please complete the form.");
         } else {
             try {
-                Student student = StudentController.createStudent(Constants.Title.valueOf(title), forename, surname, password, Constants.AccountType.STUDENT, personalTutor);
+                Student student = StudentController.createStudent(Constants.Title.valueOf(title), forename, surname,
+                        password, Constants.AccountType.STUDENT, personalTutor);
                 RegistrationController.createInitialRegistration(student.getRegistrationNumber(), degreeCode);
-                rootFrame.showMessage("Student has been registered:" + System.lineSeparator() + "Email: " + student.getEmail() + System.lineSeparator() + "Registration Number: " + student.getRegistrationNumber());
+                rootFrame.showMessage(
+                        "Student has been registered:" + System.lineSeparator() + "Email: " + student.getEmail()
+                                + System.lineSeparator() + "Registration Number: " + student.getRegistrationNumber());
                 rootFrame.moveToRegistrarDashboard();
             } catch (Exception err) {
                 rootFrame.showError("There was an error. Please try again.");
@@ -70,19 +76,19 @@ public class StudentSignUp extends JPanel {
         goBackButton = new JButton();
         panel3 = new JPanel();
         label1 = new JLabel();
-        label4 = new JLabel();
-        scrollPane1 = new JScrollPane();
-        titleComboBox = new JComboBox<>();
-        passwordBox = new JPasswordField();
         label2 = new JLabel();
-        label5 = new JLabel();
+        scrollPane1 = new JScrollPane();
+        titleComboBox = new JComboBox<String>();
         forenameBox = new JTextField();
-        personalTutorBox = new JTextField();
         label3 = new JLabel();
-        label6 = new JLabel();
+        label5 = new JLabel();
         surnameBox = new JTextField();
+        personalTutorBox = new JTextField();
+        label4 = new JLabel();
+        label6 = new JLabel();
+        passwordBox = new JPasswordField();
         scrollPane2 = new JScrollPane();
-        degreeComboBox = new JComboBox<>();
+        degreeComboBox = new JComboBox<String>();
         createStudentButton = new JButton();
 
         // ======== this ========
@@ -108,6 +114,7 @@ public class StudentSignUp extends JPanel {
         {
             panel3.setLayout(new GridBagLayout());
             ((GridBagLayout) panel3.getLayout()).columnWidths = new int[] { 105, 100 };
+            ((GridBagLayout) panel3.getLayout()).rowHeights = new int[] { 0, 0, 0, 33, 0, 0, 0, 0, 0, 0 };
 
             // ---- label1 ----
             label1.setText("Title:");
@@ -115,10 +122,10 @@ public class StudentSignUp extends JPanel {
             panel3.add(label1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 5), 0, 0));
 
-            // ---- label4 ----
-            label4.setText("Password:");
-            label4.setFont(new Font("Tahoma", Font.BOLD, 11));
-            panel3.add(label4, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+            // ---- label2 ----
+            label2.setText("Forename:");
+            label2.setFont(new Font("Tahoma", Font.BOLD, 11));
+            panel3.add(label2, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
 
             // ======== scrollPane1 ========
@@ -127,13 +134,13 @@ public class StudentSignUp extends JPanel {
             }
             panel3.add(scrollPane1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 5), 0, 0));
-            panel3.add(passwordBox, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+            panel3.add(forenameBox, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
 
-            // ---- label2 ----
-            label2.setText("Forename:");
-            label2.setFont(new Font("Tahoma", Font.BOLD, 11));
-            panel3.add(label2, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+            // ---- label3 ----
+            label3.setText("Surname:");
+            label3.setFont(new Font("Tahoma", Font.BOLD, 11));
+            panel3.add(label3, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 5), 0, 0));
 
             // ---- label5 ----
@@ -141,15 +148,15 @@ public class StudentSignUp extends JPanel {
             label5.setFont(new Font("Tahoma", Font.BOLD, 11));
             panel3.add(label5, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
-            panel3.add(forenameBox, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+            panel3.add(surnameBox, new GridBagConstraints(0, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 5), 0, 0));
             panel3.add(personalTutorBox, new GridBagConstraints(1, 3, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
 
-            // ---- label3 ----
-            label3.setText("Surname:");
-            label3.setFont(new Font("Tahoma", Font.BOLD, 11));
-            panel3.add(label3, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+            // ---- label4 ----
+            label4.setText("Password:");
+            label4.setFont(new Font("Tahoma", Font.BOLD, 11));
+            panel3.add(label4, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 5), 0, 0));
 
             // ---- label6 ----
@@ -157,7 +164,7 @@ public class StudentSignUp extends JPanel {
             label6.setFont(new Font("Tahoma", Font.BOLD, 11));
             panel3.add(label6, new GridBagConstraints(1, 4, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 0), 0, 0));
-            panel3.add(surnameBox, new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
+            panel3.add(passwordBox, new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER,
                     GridBagConstraints.BOTH, new Insets(0, 0, 5, 5), 0, 0));
 
             // ======== scrollPane2 ========
