8c1603df9baa07990d18699ee266182471b74ece
Dom Barter <dominicbarter@gmail.com>
2020-11-28 18:16:19 +0000

    Began template data full

--- src/database/TemplateData.java
+++ src/database/TemplateData.java
@@ -117,4 +117,89 @@ public class TemplateData {
 
     }
 
+    public static void fullData() {
+
+        // Insert some users
+        try {
+            UserController.createUser(Constants.Title.MR, "Brian", "Smith", "password", Constants.AccountType.TEACHER);
+            UserController.createUser(Constants.Title.MR, "James", "Smith", "password",
+                    Constants.AccountType.REGISTRAR);
+            UserController.createUser(Constants.Title.MR, "David", "Grey", "password",
+                    Constants.AccountType.ADMINISTRATOR);
+        } catch (Exception e) {
+            System.out.println("There was an error with user creation: " + e);
+        }
+
+        // Create some departments
+        try {
+            DepartmentController.createDepartment("BUS", "Business School");
+            DepartmentController.createDepartment("COM", "Computer Science");
+            DepartmentController.createDepartment("PSY", "Psychology");
+            DepartmentController.createDepartment("LAN", "Modern Languages");
+        } catch (Exception e) {
+            System.out.println("There was an error with department creation: " + e);
+        }
+
+        // Create some degrees
+        try {
+            DegreeController.createDegree("BUSU01", "MSc in Business Administration", false, 4);
+            DepartmentController.createDegreeDepartment("BUS", "BUSU01", true);
+
+            DegreeController.createDegree("COMU01", "MEng Software Engineering with a Year in Industry", true, 4);
+            DepartmentController.createDegreeDepartment("COM", "COMU01", true);
+
+            DegreeController.createDegree("COMU02", "BSc Information Systems", false, 3);
+            DepartmentController.createDegreeDepartment("COM", "COMU02", true);
+            DepartmentController.createDegreeDepartment("BUS", "COMU02", false);
+            DepartmentController.createDegreeDepartment("LAN", "COMU02", false);
+
+            DegreeController.createDegree("PSYU01", "MPsy Cognitive Science", false, 4);
+            DepartmentController.createDegreeDepartment("PSY", "PSYU01", true);
+            DepartmentController.createDegreeDepartment("COM", "PSYU01", false);
+
+            // One year degree for testing
+            DegreeController.createDegree("PSYP01", "MPsy Cognitive Science", false, 4);
+            DepartmentController.createDegreeDepartment("PSY", "PSYP01", true);
+            DepartmentController.createDegreeDepartment("COM", "PSYP01", false);
+        } catch (Exception e) {
+            System.out.println("There was an error with degree creation: " + e);
+        }
+
+        // Create some modules
+        try {
+
+            // BUSU01
+
+            // COMU01
+
+            // COMU02
+
+            // 
+
+            ModuleController.createModule("COM1001", "Intro to software engineering 1", 20, "AUTUMN");
+            ModuleController.createModule("COM1002", "Intro to software engineering 2", 20, "AUTUMN");
+            ModuleController.createModule("COM1003", "Intro to software engineering 3", 20, "AUTUMN");
+            ModuleController.createModule("COM1004", "Intro to software engineering 4", 20, "AUTUMN");
+            ModuleController.createModule("COM1005", "Intro to software engineering 5", 20, "AUTUMN");
+            ModuleController.createModule("COM1006", "Intro to software engineering 6", 20, "AUTUMN");
+            ModuleController.createModule("COM1007", "Intro to software engineering 7", 20, "AUTUMN");
+        } catch (Exception e) {
+            System.out.println("There was an error with module creation: " + e);
+        }
+
+        // Link some modules
+        try {
+            DegreeController.createDegreeModule("COMU01", "COM1001", false, "1");
+            DegreeController.createDegreeModule("COMU01", "COM1002", false, "1");
+            DegreeController.createDegreeModule("COMU01", "COM1003", false, "1");
+            DegreeController.createDegreeModule("COMU01", "COM1004", false, "1");
+            DegreeController.createDegreeModule("COMU01", "COM1005", false, "1");
+            DegreeController.createDegreeModule("COMU01", "COM1006", false, "1");
+            DegreeController.createDegreeModule("COMU01", "COM1007", false, "1");
+        } catch (Exception e) {
+            System.out.println("There was an error with module linking: " + e);
+        }
+
+    }
+
 }
